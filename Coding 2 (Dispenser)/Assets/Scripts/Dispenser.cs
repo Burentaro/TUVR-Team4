1a3e6770c9bfc50022cc9a7ee194a27e351db917
Giigee <egiiguulen@gmail.com>
2019-02-08 17:36:12 +0900

    Added coin

--- Coding 2 (Dispenser)/Assets/Scripts/Dispenser.cs
+++ Coding 2 (Dispenser)/Assets/Scripts/Dispenser.cs
@@ -10,20 +10,31 @@ public class Dispenser : MonoBehaviour
     public AudioClip audioClip;
     public AudioSource audioSource;
 
+    public bool hasInsertedCoin = false;
+
     /// <summary>
     /// Use this method to dispense the item.
     /// </summary>
     /// <param name="itemNumber">ID of the item in the </param>
     public void Dispense(int itemNumber)
     {
-        audioSource.PlayOneShot(audioClip,1);
+        if (hasInsertedCoin == true)
+        {
+            audioSource.PlayOneShot(audioClip, 1);
 
-        // Dispense an object here using the Instantiate<> function
-        //Dispense an object here  Instantiate<GameObject>  urgeljluul
-        GameObject newGameObject = Instantiate<GameObject>(inventoryItems[itemNumber]);
+            // Dispense an object here using the Instantiate<> function
+            //Dispense an object here  Instantiate<GameObject>  urgeljluul
+            GameObject newGameObject = Instantiate<GameObject>(inventoryItems[itemNumber]);
 
-        newGameObject.transform.position = dispensePosition.position;
+            newGameObject.transform.position = dispensePosition.position;
 
-        //Debug.Log("Thilo raises hand" + item ); for testing
+            //Debug.Log("Thilo raises hand" + item ); for testing
+            hasInsertedCoin = false;
+        }
+    }
+
+    public void InsertCoin(int value)
+    {
+        hasInsertedCoin = true;
     }
 }
