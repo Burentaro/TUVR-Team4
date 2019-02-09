9e189195b604947ccc49cc5d1d08343d62950cef
gareau <thilo@asablackbox.com>
2019-02-09 14:37:26 +0900

    SteamVR Input files

---
+++ New SteamVR Input/Assets/Scripts/BlackboardUserInterface.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BlackboardUserInterface : MonoBehaviour
+{
+    public Text targetDistanceText; // Reference to the UI Text for showing distance to target
+    public Text shotDistanceText;   // Reference to the UI Text for showing shot distance
+    public Text cannonballText;     // Reference to the UI Text for showing what cannonball was loaded
+    public Text powderText;         // Reference to the UI Text for showing how much powder was loaded
+    public Text angleText;          // Reference to the UI Text for showing last angle used
+
+    public void OnTargetMissed(ShotHistory history)
+    {
+        // Update Text fields
+        SetTextData(SceneManager.Instance.GetTargetDistance(), history);
+    }
+
+    public void OnTargetHit(ShotHistory history)
+    {
+        // Update Text fields
+        SetTextData(SceneManager.Instance.GetTargetDistance(), history);
+    }
+
+    private void SetTextData(float targetDistance, ShotHistory history)
+    {
+        // TO-DO!!
+        // Update the text field in the UI Text with the ShotHistory
+    }
+}
---
+++ New SteamVR Input/Assets/Scripts/Target.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : Actor
+{
+    public GameObject explodingObject;  // A reference to the object that we will use to show an explosion.
+    public Animator explodingAnimator;  // Animator that controls the explosion animation
+
+    private void Awake()
+    {
+        // TO-DO!!
+        // Hide all objects by disabling all MeshRenders
+    }
+
+    /// <summary>
+    /// Called when you want to replace this object with another one
+    /// </summary>
+    public void Replace()
+    {
+        // TO-DO!!
+        // Hide this object
+
+        // TO-DO!!
+        // Show the expoloding object
+
+        // TO-DO!! CHALLENGE!!!
+        // Start the exploding animation by playing the explosion state on the object
+
+        // TO-DO!!
+        // Remove this from the scene with 2 second delay
+    }
+
+    /// <summary>
+    /// Shows or hides an object by enabling or disabling the MeshRender component on all child objects
+    /// </summary>
+    /// <param name="value">True to show. False to hide.</param>
+    /// <param name="target">Object you want to hide.</param>
+    private void SetMeshRenderer(bool value, GameObject target)
+    {
+        // Get all of the MeshRenderes on the object's children
+        MeshRenderer[] meshRenderers = target.GetComponentsInChildren<MeshRenderer>();
+        
+        // Only continue if there are any MeshRenderes on the children
+        if (meshRenderers != null && meshRenderers.Length > 0)
+        {
+            // Go through all MeshRenderes in the array and set their values
+            foreach (MeshRenderer meshRenderer in meshRenderers)
+            {
+                meshRenderer.enabled = value;
+            }
+        }
+    }
+}
