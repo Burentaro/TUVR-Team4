1764583e0a10a91a14c7600d6c6ad09061585af0
Chikara00 <hasimoto002002@gmail.com>
2019-02-12 12:16:30 +0900

    my project

---
+++ my project 3/Assets/Scenes/scripts/script/TempName.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempName : MonoBehaviour
+{
+    public Rigidbody shiroiBoru;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            print("space key was pressed");
+            shiroiBoru.AddForce(new Vector3(-100, 0, 0));
+        }
+    }
+}
